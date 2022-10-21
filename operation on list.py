63041cd149649908fc92da416a9ac2d29bd5cde2
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2022-10-21 12:49:43 -0700

    Create operation on list.py

---
+++ operation on list.py
@@ -0,0 +1,11 @@
+lst = ['Data', 10, 5.26, 'Science']
+print(lst)
+
+lst.append(50)                          # insert the 50 at the last element of list
+print(lst)
+
+lst.insert(1, ['New', 10])                    # insert the element at specified location
+print(lst)
+
+lst.extend([2, 'ATAL', 3.14])           # insert multiple elements together
+print(lst)
